4b4989dba1566065c0df02d682bd772f3b2bdafb
Carlos <geraldocarlos503@gmai.com>
2026-02-28 18:43:38 -0300

    teste

--- src/main/java/dev/zeroone/CadastroDeUsuarios/CadastroDeUsuariosApplication.java
+++ src/main/java/dev/zeroone/CadastroDeUsuarios/CadastroDeUsuariosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroDeUsuariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroDeUsuariosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeUsuariosApplication.class, args);
+    }
 
 }
---
+++ src/main/java/dev/zeroone/CadastroDeUsuarios/Controller.java
@@ -0,0 +1,15 @@
+package dev.zeroone.CadastroDeUsuarios;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa e a minha primeira rota";
+    }
+}
