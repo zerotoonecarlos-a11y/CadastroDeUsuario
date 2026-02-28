bf3a85a820e00c1c0b00a24a131c541b358af39e
Carlos <geraldocarlos503@gmai.com>
2026-02-28 15:54:13 -0300

    teste

---
+++ src/main/java/dev/zeroone/CadastroDeUsuarios/CadastroDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package dev.zeroone.CadastroDeUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zeroone/CadastroDeUsuarios/CadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zeroone.CadastroDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
